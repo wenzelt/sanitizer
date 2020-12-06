d9e3e4d3340b82645ad9a9ba25ddc0290b8004c8
twenzel <tom.wenzel@tum.de>
2020-12-06 01:35:20 +0100

    formatting

--- JS/sanitizer_v0.01.js
+++ JS/sanitizer_v0.01.js
@@ -4,10 +4,12 @@
             document.getElementById('droplet').style.animation = "drawArc 1.0s reverse ease-in 1";
             document.getElementById('droplet2').style.animation = "drawArc2 1.0s reverse ease-in 1";
             document.getElementById('droplet3').style.animation = "drawArc3 1.0s reverse ease-in 1";
-            setTimeout(() => { document.getElementById('nozzle').style.animation = "";
+            setTimeout(() => {
+                document.getElementById('nozzle').style.animation = "";
 
-            document.getElementById('droplet').style.animation = "";
-            document.getElementById('droplet2').style.animation = "";
-            document.getElementById('droplet3').style.animation = "";}, 1000);
+                document.getElementById('droplet').style.animation = "";
+                document.getElementById('droplet2').style.animation = "";
+                document.getElementById('droplet3').style.animation = "";
+            }, 1000);
 
         };
